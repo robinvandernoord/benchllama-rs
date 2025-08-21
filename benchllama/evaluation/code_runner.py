86a353a062e0bcc4a613c7e1e7a0a4a4f7cb636e
Robin van der Noord <robinvandernoord@gmail.com>
2025-08-21 13:29:45 +0200

    feat: add Rust test runner support

--- benchllama/evaluation/code_runner.py
+++ benchllama/evaluation/code_runner.py
@@ -1,12 +1,11 @@
 import pandas as pd
 from pathlib import Path
 
-
 from ..constants import Result
 from .runners.python_runner import PythonRunner
 
 from .runners.cpp_runner import CppRunner
-# from .runners.rust_runner import RustRunner
+from .runners.rust_runner import RustRunner
 from .runners.java_runner import JavaRunner
 from .runners.javascript_runner import JavascriptRunner
 from .runners.go_runner import GoRunner
@@ -15,22 +14,21 @@ from .runners.go_runner import GoRunner
 class CodeRunner:
     def __init__(self, execution_dir: Path):
         self.execution_dir = execution_dir
-        self.python_runner = PythonRunner(execution_dir)
-        self.javascript_runner = JavascriptRunner(execution_dir)
-        self.java_runner = JavaRunner(execution_dir)
-        self.cpp_runner = CppRunner(execution_dir)
-        self.go_runner = GoRunner(execution_dir)
+
+        self.runners = {
+            "cpp": CppRunner(execution_dir),
+            "go": GoRunner(execution_dir),
+            "javacript": JavascriptRunner(execution_dir),
+            "java": JavaRunner(execution_dir),
+            "python": PythonRunner(execution_dir),
+            "rust": RustRunner(execution_dir),
+        }
 
     def run(self, problem: pd.Series):
-        if problem["language"].lower() == "python":
-            return self.python_runner.run(problem)
-        elif problem["language"].lower() == "cpp":
-            return self.cpp_runner.run(problem)
-        elif problem["language"].lower() == "java":
-            return self.java_runner.run(problem)
-        elif problem["language"].lower() == "javascript":
-            return self.javascript_runner.run(problem)
-        elif problem["language"].lower() == "go":
-            return self.go_runner.run(problem)
-        else:
+        language = problem["language"].lower()
+
+        if language not in self.runners:
             return Result.FAILURE, "Language not supported!"
+
+        print('runner for', language)
+        return self.runners[language].run(problem)
